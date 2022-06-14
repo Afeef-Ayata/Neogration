9fe389d4b0427a4ad6552de47b6bbc7b802d72b7
Afeef k k <afeef.abdulla@neopraxis.in>
2022-06-14 15:11:59 +0530

    meaningful msg for login errors

--- project/auth.py
+++ project/auth.py
@@ -22,9 +22,12 @@ def login_post():
 
     # check if the user actually exists
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
-    if not user or not check_password_hash(user.password, password):
-        flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+    if not user:
+        flash("Oh! No!, You havn't signed up using this mail id")
+        return redirect(url_for('auth.login')) # if the user doesn't exist reload the page
+    elif not check_password_hash(user.password, password):
+        flash("Oh! Sorry!, Your password don't match with our records")
+        return redirect(url_for('auth.login')) # if the password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
     return redirect(url_for('main.profile'))
