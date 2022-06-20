7c2a5a6f3ce7e74c1140adf982f6b516e388771b
Afeef k k <afeef.abdulla@neopraxis.in>
2022-06-20 13:26:49 +0530

    Added dotenv package

--- project/editor.py
+++ project/editor.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 from .mongoDb import get_database
 
 editor = Blueprint('editor', __name__)
 
 @editor.route('/editor')
+@login_required
 def index():
     db = get_database('IntegrationDb')
 
--- project/mongoDb.py
+++ project/mongoDb.py
@@ -1,8 +1,12 @@
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
 # This file wil the core mongodb management client
 
 def get_database(name):
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = "mongodb+srv://AfeefNeo:qwerty123@cluster0.2p5oz.mongodb.net/?retryWrites=true&w=majority"
+    CONNECTION_STRING = config["ATLAS_URI"]
 
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     from pymongo import MongoClient
