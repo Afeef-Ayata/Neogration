0dc7bdc8361713db76ea22894184fa678494958e
Afeef k k <afeef.abdulla@neopraxis.in>
2022-06-22 16:36:44 +0530

    saving NeoScript to mongodb collections

--- project/editor.py
+++ project/editor.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required
 from .mongoDb import get_database
 
 from project.models import NeoScript
@@ -21,6 +21,24 @@ def index():
         print(item)
 
     return render_template('editor.html')
+
+@editor.route('/editor', methods=['POST'])
+@login_required
+def create_new_script():
+    name = request.form.get('name')
+
+    new_script = NeoScript(
+        name = name,
+        description = request.form.get('description'),
+        author = current_user.name,
+        script = request.form.get('scriptarea'),
+    )
+
+    new_script.save()
+
+    flash(f"Your script {name} is saved into mongodb")
+    
+    return redirect(url_for('main.success'))
 
 @editor.route("/create")
 def add_new_script():
